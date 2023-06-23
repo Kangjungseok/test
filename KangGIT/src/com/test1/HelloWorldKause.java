89bc459eeb15c38d7bddfa600de40b4aeda533ff
kimhankyu <131732322+Star-kyu@users.noreply.github.com>
2023-06-23 17:37:45 +0900

    ㅁㄴㅇ

--- KangGIT/src/com/test1/HelloWorldKause.java
+++ KangGIT/src/com/test1/HelloWorldKause.java
@@ -5,6 +5,9 @@ public class HelloWorldKause {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕 Kang");
+		System.out.println("잘가 Kang");
+		
+		
 	}
 
 }
